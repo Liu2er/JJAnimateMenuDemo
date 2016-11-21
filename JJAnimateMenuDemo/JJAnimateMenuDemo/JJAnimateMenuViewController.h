24b16c99d98109d30df27232cafae52d55e2be84
liujiajie <liujiajie@dianping.com>
2016-11-21 16:48:32 +0800

    【添加JJAnimateMenuViewController】

---
+++ JJAnimateMenuDemo/JJAnimateMenuDemo/JJAnimateMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJAnimateMenuViewController.h
+//  JJAnimateMenuDemo
+//
+//  Created by 刘佳杰 on 16/11/21.
+//  Copyright © 2016年 Jiajie.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAnimateMenuViewController : UIViewController
+
+@end
