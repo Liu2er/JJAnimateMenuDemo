9bbb672f49776af4964504918757ddf9c299c800
liujiajie <liujiajie@dianping.com>
2016-11-22 10:02:04 +0800

    【添加JJMenuViewController】

---
+++ JJAnimateMenuDemo/JJAnimateMenuDemo/JJMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJMenuViewController.h
+//  JJAnimateMenuDemo
+//
+//  Created by 刘佳杰 on 16/11/22.
+//  Copyright © 2016年 Jiajie.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJMenuViewController : UIViewController
+
+@end
---
+++ JJAnimateMenuDemo/JJAnimateMenuDemo/JJMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  JJMenuViewController.m
+//  JJAnimateMenuDemo
+//
+//  Created by 刘佳杰 on 16/11/22.
+//  Copyright © 2016年 Jiajie.Liu. All rights reserved.
+//
+
+#import "JJMenuViewController.h"
+
+@interface JJMenuViewController ()
+
+@end
+
+@implementation JJMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
